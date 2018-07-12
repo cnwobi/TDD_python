2dcf5f478846bafad86238b88d2c8a8b0a712dc6
Chuka <c.nwobi@outlook.com>
2018-07-12 22:08:32 +1000

    URLs for forms x2, moved code in views + tests, new URL

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -3,9 +3,9 @@ from lists.models import Item
 
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/one_list_only/')
+    # if request.method == 'POST':
+    #     Item.objects.create(text=request.POST['item_text'])
+    #     return redirect('/lists/one_list_only/')
 
     return render(request, 'home.html')
 
@@ -15,3 +15,8 @@ def home_page(request):
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/one_list_only/')
